d0926d271d72d18296bf3aeedda82852c813ca29
Olivier Roques <oroques@cisco.com>
2021-01-29 10:11:06 +0100

    Improve buffer detection

--- lua/bufdel.lua
+++ lua/bufdel.lua
@@ -24,6 +24,18 @@ local function get_next(buf)
   end
 end
 
+local function get_buffer(bufexpr)
+  if not bufexpr then
+    return fn.bufnr()
+  end
+  if tonumber(bufexpr) then
+    return tonumber(bufexpr)
+  end
+  bufexpr = string.gsub(bufexpr, [[^['"]+]], '')
+  bufexpr = string.gsub(bufexpr, [[['"]+$]], '')
+  return fn.bufnr(bufexpr)
+end
+
 local function delete_buffer(bufexpr, force)
   if #fn.getbufinfo({buflisted = 1}) < 2 then
     if force then
@@ -33,17 +45,20 @@ local function delete_buffer(bufexpr, force)
     end
     return
   end
-  local cur_buf = type(bufexpr) == 'number' and bufexpr or fn.bufnr(bufexpr)
-  local next_buf = get_next(cur_buf)
-  local windows = fn.getbufinfo(cur_buf)[1].windows
-  switch_buffer(windows, next_buf)
-  if force or fn.getbufvar(cur_buf, '&buftype') == 'terminal' then
-    cmd(string.format('bd! %d', cur_buf))
-  else
-    cmd(string.format('silent! confirm bd %d', cur_buf))
+  local buf = get_buffer(bufexpr)
+  if fn.buflisted(buf) == 0 then
+    return
   end
-  if fn.buflisted(cur_buf) == 1 then
-    switch_buffer(windows, cur_buf)
+  local next_buf = get_next(buf)
+  local windows = fn.getbufinfo(buf)[1].windows
+  switch_buffer(windows, next_buf)
+  if force or fn.getbufvar(buf, '&buftype') == 'terminal' then
+    cmd(string.format('bd! %d', buf))
+  else
+    cmd(string.format('silent! confirm bd %d', buf))
+  end
+  if fn.buflisted(buf) == 1 then
+    switch_buffer(windows, buf)
   end
 end
 
